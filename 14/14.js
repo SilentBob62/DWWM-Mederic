c0a426d124e8c2f0b2386f539d2ba9e6388dc085
médéric maquinghen <maqmed@msn.com>
2020-04-21 15:22:52 +0200

    14 v3

--- 14/14.js
+++ 14/14.js
@@ -1,23 +1,37 @@
-menuTitre1=document.getElementsByClassName("menuTitre")[0];
-menuTitre2=document.getElementsByClassName("menuTitre")[1];
-sousMenu1=document.getElementsByClassName("sousMenuGroupe")[0];
-sousMenu2=document.getElementsByClassName("sousMenuGroupe")[1];
+d1 = document.getElementById("d1");
+d2 = document.getElementById("d2");
 
-menuTitre1.addEventListener("click",function(){
-    menuApparait(sousMenu1);
-});
-menuTitre2.addEventListener("click",function(){
-    menuApparait(sousMenu2);
-});
+p1=document.getElementsByClassName("sousMenu1");
+p2=document.getElementsByClassName("sousMenu2");
 
-
-function menuApparait(sousMenu){
-    if (sousMenu.visibility=="hidden")
-	{
-        sousMenu.visibility = "visible"; 
-    }
-    else
-    {
-        sousMenu.visibility = "hidden"
-    }
+for (i=0;i<p1.length;i++){
+    p1[i].style.display="none";
 }
+for (i=0;i<p2.length;i++){
+    p2[i].style.display="none";
+}
+
+
+d1.addEventListener("mouseover", () => {
+    for (i=0;i<p1.length;i++){
+    p1[i].style.display="block";
+}
+});
+
+d1.addEventListener("mouseout", () => {
+for (i=0;i<p1.length;i++){
+    p1[i].style.display="none";
+}
+});
+
+d2.addEventListener("mouseover", () => {
+    for (i=0;i<p2.length;i++){
+    p2[i].style.display="block";
+}
+});
+
+d2.addEventListener("mouseout", () => {
+for (i=0;i<p2.length;i++){
+    p2[i].style.display="none";
+}
+});
