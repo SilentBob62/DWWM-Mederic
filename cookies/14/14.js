239364018c828bc4164e9ba98ad9abc732f88bb6
médéric maquinghen <maqmed@msn.com>
2020-04-22 14:12:12 +0200

    modif js

--- cookies/14/14.js
+++ cookies/14/14.js
@@ -1,3 +1,15 @@
+/**fonctions */
+/*#########################################*/
+function cacherSousMenu(p){
+    for (i=0;i<p.length;i++){
+        p[i].style.display="none";
+    }
+}
+function montrerSousMenu(p){
+    for (i=0;i<p.length;i++){
+        p[i].style.display="block";
+    } 
+}
 /**recuperer les elements */
 /*#########################################*/
 d1 = document.getElementById("d1");
@@ -14,35 +26,51 @@ cacherSousMenu(p3);
 /**Pour les actions */
 /*#########################################*/
 /**pour le premier menu */
-d1.addEventListener("mouseover", function ()  {
-    montrerSousMenu(p1);
-});
-d1.addEventListener("mouseout", function () {
-    cacherSousMenu(p1);
+d1.addEventListener("click", function ()  {
+    if (p1[0].style.display=="none"){
+        montrerSousMenu(p1);
+    }
+    else{
+        cacherSousMenu(p1);
+    }
 });
 /**pour le deuxieme menu */
-d2.addEventListener("mouseover", function () {
-    montrerSousMenu(p2);
-});
-d2.addEventListener("mouseout", function () {
-    cacherSousMenu(p2);
+d2.addEventListener("click", function ()  {
+    if (p2[0].style.display=="none"){
+        montrerSousMenu(p2);
+    }
+    else{
+        cacherSousMenu(p2);
+    }
 });
 /**pour le troisieme menu */
-d3.addEventListener("mouseover", function () {
-    montrerSousMenu(p3);
-});
-d3.addEventListener("mouseout", function () {
-    cacherSousMenu(p3);
-});
-/**fonctions */
-/*#########################################*/
-function cacherSousMenu(p){
-    for (i=0;i<p.length;i++){
-        p[i].style.display="none";
+d3.addEventListener("click", function ()  {
+    if (p3[0].style.display=="none"){
+        montrerSousMenu(p3);
     }
-}
-function montrerSousMenu(p){
-    for (i=0;i<p.length;i++){
-        p[i].style.display="block";
-    } 
-}
+    else{
+        cacherSousMenu(p3);
+    }
+});
+/**avec mouse over*/
+/**pour le premier menu */
+//d1.addEventListener("mouseover", function () {
+//     montrerSousMenu(p1);
+// });
+// d1.addEventListener("mouseout", function () {
+//     cacherSousMenu(p1);
+// });
+/**pour le deuxieme menu */
+// d2.addEventListener("mouseover", function () {
+//     montrerSousMenu(p2);
+// });
+// d2.addEventListener("mouseout", function () {
+//     cacherSousMenu(p2);
+// });
+/**pour le troisieme menu */
+// d3.addEventListener("mouseover", function () {
+//     montrerSousMenu(p3);
+// });
+// d3.addEventListener("mouseout", function () {
+//     cacherSousMenu(p3);
+// });
