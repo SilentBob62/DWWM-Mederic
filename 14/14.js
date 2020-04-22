dd4677014ed217263ff6c13b0c0a9f7d83e50f86
médéric maquinghen <maqmed@msn.com>
2020-04-22 10:58:31 +0200

    plus de menu

--- 14/14.js
+++ 14/14.js
@@ -1,8 +1,10 @@
 d1 = document.getElementById("d1");
 d2 = document.getElementById("d2");
+d3 = document.getElementById("d3");
 
 p1=document.getElementsByClassName("sousMenu1");
 p2=document.getElementsByClassName("sousMenu2");
+p3=document.getElementsByClassName("sousMenu3");
 
 for (i=0;i<p1.length;i++){
     p1[i].style.display="none";
@@ -10,7 +12,9 @@ for (i=0;i<p1.length;i++){
 for (i=0;i<p2.length;i++){
     p2[i].style.display="none";
 }
-
+for (i=0;i<p3.length;i++){
+    p3[i].style.display="none";
+}
 
 d1.addEventListener("mouseover", () => {
     for (i=0;i<p1.length;i++){
@@ -33,5 +37,17 @@ d2.addEventListener("mouseover", () => {
 d2.addEventListener("mouseout", () => {
 for (i=0;i<p2.length;i++){
     p2[i].style.display="none";
+}
+});
+
+d3.addEventListener("mouseover", () => {
+    for (i=0;i<p3.length;i++){
+    p3[i].style.display="block";
+}
+});
+
+d3.addEventListener("mouseout", () => {
+for (i=0;i<p3.length;i++){
+    p3[i].style.display="none";
 }
 });
