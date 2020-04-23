eaf7d80c95a160de537924fc0fbe39ee4c4ec8ae
médéric maquinghen <maqmed@msn.com>
2020-04-23 16:39:57 +0200

    modif page

--- Ajax/velib/php/View/Footer.php
+++ Ajax/velib/php/View/Footer.php
@@ -1,17 +1,13 @@
 <!--  Referme les balises ouvertes dans le Header -->
 
-</div>
-<div class="piedDePage">
-<div class="espaceHorizon"></div>
-<div class="espaceHorizon"></div>
-<div class="espaceHorizon"></div>
-<div class="signature">Réalisé par SilentB0B</div>
-
-
-</div>
-</div>
-</div>
-<script src="JS/ajax.js"></script>
+            </div>
+            <div class="piedDePage">
+                <div class="espaceHorizon2"></div>
+                <div class="signature">Réalisé par SilentB0B</div>
+            </div>
+        </div>
+    </div>
+    <script src="JS/ajax.js"></script>
 </body>
 
 </html>
--- Ajax/velib/php/View/Header.php
+++ Ajax/velib/php/View/Header.php
@@ -6,7 +6,6 @@
 				<div id="logo">
 			 	<img src="Images/logo.png" />
 				</div>
-
 				<div id="titre_page">
                   <h1>velib de Lille</h1>
                 </div>
