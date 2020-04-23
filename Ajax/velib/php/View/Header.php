e392da9456b627908d1ae28e85fef70c04b906bf
médéric maquinghen <maqmed@msn.com>
2020-04-23 14:23:23 +0200

    velib

---
+++ Ajax/velib/php/View/Header.php
@@ -0,0 +1,15 @@
+
+<body>
+	<div id="page">
+		<div id="entete">
+			<div id="titre">
+				<div id="logo">
+			 	<img src="Images/logo.png" />
+				</div>
+
+				<div id="titre_page">
+                  <h1>velib de Lille</h1>
+                </div>
+            </div>
+		</div>
+        <div id="contenu">
---
+++ Ajax/velib/php/View/Principale.php
@@ -0,0 +1,13 @@
+<!-- Cette page est le contenu de la page principale du site; Elle présente la liste des parkings. -->
+
+<div id="divSousTitre">
+	<h3>Selectionner un lieu</h3>
+</div>
+<div id="divContenu">
+	<div class="ligne titreLigne">
+		<div class="ville">Ville</div>
+		<div class="libelle">Nom du lieu</div>
+		<div class="dispo">Etat</div>
+	</div>
+	<div class="espaceHorizon"></div>
+</div>
